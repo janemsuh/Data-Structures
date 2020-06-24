dc3a9f61c39ce6a6163e2225ce8efbd25ddc6fed
Jane Suh <sourcherryyogurt@gmail.com>
2020-06-23 22:25:33 -0400

    Implemented get_max and for_each methods in BSTNode class

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -49,11 +49,18 @@ class BSTNode:
 
     # Return the maximum value found in the tree
     def get_max(self):
-        pass
+        if self.right:
+            return self.right.get_max()
+        return self.value
 
     # Call the function `fn` on the value of each node
     def for_each(self, fn):
-        pass
+        fn(self.value)
+        if self.left:
+            self.left.for_each(fn)
+        if self.right:
+            self.right.for_each(fn)
+
 
     # Part 2 -----------------------
 
