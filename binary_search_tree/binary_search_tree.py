dabf5acaaedbdc15eb56856b21d6c5f35e5a1e1d
Jane Suh <sourcherryyogurt@gmail.com>
2020-06-23 21:38:16 -0400

    Implemented insert and contains methods in BSTNode class

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -17,12 +17,35 @@ class BSTNode:
 
     # Insert the given value into the tree
     def insert(self, value):
-        pass
+        if value < self.value:
+            if self.left:
+                self.left.insert(value)    
+            else:
+                self.left = BSTNode(value)  
+        else:
+            if self.right:
+                self.right.insert(value)
+            else:
+                self.right = BSTNode(value)
 
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
-        pass
+        if self.value == target:
+            return True
+        
+        found = False
+        if self.value > target:
+            if self.left is None:
+                return False
+            found = self.left.contains(target)
+        
+        else:
+            if self.right is None:
+                return False
+            found = self.right.contains(target)
+        
+        return found
 
     # Return the maximum value found in the tree
     def get_max(self):
